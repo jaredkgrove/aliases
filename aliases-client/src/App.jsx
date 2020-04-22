16d7a76ab41dfd6c54c84cf97bc7afd0abfd9faf
jaredkgrove <jaredkgrove@gmail.com>
2020-04-21 22:10:10 -0500

    viewport test

--- aliases-client/src/App.jsx
+++ aliases-client/src/App.jsx
@@ -26,6 +26,7 @@ function App () {
 
     return (
       <Layout>
+        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86"></meta>
         <Switch>
           <Route exact path="/">
             <RoomJoin socket={socket} handleGameJoin={handleGameJoin}></RoomJoin>
